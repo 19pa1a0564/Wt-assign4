19e77c1e8d353388464f890d7343e4477f54a194
19pa1a0564 <60499483+19pa1a0564@users.noreply.github.com>
2021-07-12 14:40:35 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
        $password=$_POST["password"];
        $email=$_POST["email"];
        $phone=$_POST["phone"];
-       $qry1="INSERT INTO data(`name`, `password`, `email`, `phone`) VALUES ('$name','$password','$email','$phone')";
+       $qry1="INSERT INTO regdetails(`name1`, `password1`, `email1`, `phone1`) VALUES ('$name','$password','$email','$phone')";
        $res1=mysqli_query($conn,$qry1);
     }
     mysqli_close($conn);
@@ -25,6 +25,9 @@
      Email Id : <input type="email"  id="mailid" name="email"><br><br>
      Phone No : <input type="tel" id="phone" name="phone" pattern="[0-9]{10}"  required><br><br>
      <input type="submit" value="Submit" id="submit" style="background-color:green;border-radius:30px;"></center>
+    </form>
+    <form action="result.php";>
+    <button type="submit" name="submit"; style="color:red">who filled the form</button>
     </form>
     </div></center>
 </body>
