ddda6aa313e0a6404cbfc83490be7ed7453a6050
19pa1a0564 <60499483+19pa1a0564@users.noreply.github.com>
2021-07-12 14:39:56 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username="root";
 $password="";
-$dbname="table";
+$dbname="anusha";
 $conn=mysqli_connect($servername,$username,$password,$dbname);
 if(!$conn){
     die("connection failed:".mysqli_connect_error());}
